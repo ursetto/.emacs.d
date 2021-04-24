66376c4a5896ca97365ba05d2a49565bf2499434
Jim Ursetto <jim@3e8.org>
2021-04-24 00:59:45 -0500

    Skip reading package archives at init time

--- early-init.el
+++ early-init.el
@@ -2,16 +2,21 @@
 ;; done implicitly at the end of early-init, but in earlier versions it happens at the end
 ;; of init, so do it manually now. This can also be deferred to early in init.el, but take
 ;; care to disable the implicit call here.
-(setq package-archives
+
+(setq package-enable-at-startup nil) ;; Prevent package-initialize from being called implicitly.
+(setq package-quickstart t)  ;; (ignored on Emacs < 27) M-x package-quickstart-refresh rebuilds package-quickstart.el.
+(setq package-archives '())  ;; Saves a lot of time in package-initialize, skips slow archive file read.
+(package-initialize)
+(setq package-archives       ;; Now set the archives so later installs work.
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ;;("org" . "https://orgmode.org/elpa/")
         ("melpa" . "https://melpa.org/packages/")))
-(setq package-enable-at-startup nil) ;; Prevent package-initialize from being called implicitly.
-(setq package-quickstart t)  ;; (ignored on Emacs < 27) M-x package-quickstart-refresh rebuilds package-quickstart.el.
-(package-initialize)
+;; Note: Setting package-archives correctly after init still allows init-time install of
+;; missing packages, but you get a warning that package-initialize is called twice.
+;; Probably harmless and will not recur after you restart. So far, no ill effects from
+;; setting archives to '() upfront.
 
-;; Also pull in 'use-package as we rely on it heavily.
-
+;; Pull in use-package as we rely on it heavily.
 (eval-when-compile
   (require 'use-package))
 (setq use-package-always-ensure t)
