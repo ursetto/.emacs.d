e813fe0276ddf8968ee0b55cf03c6fea4983f828
Jim Ursetto <jim@3e8.org>
2021-04-26 00:43:27 -0500

    Do not check for local package modifications at straight startup

--- init/init-package.el
+++ init/init-package.el
@@ -1,3 +1,12 @@
+;; Initialize straight.el package manager.
+
+;; straight assumes you may manually edit or pull package files, and tries to detect this
+;; at startup. This is expensive and unlikely, so remove 'find-at-startup from the
+;; modification list. Use M-x straight-check-all to recheck when needed, or see:
+;; https://github.com/raxod502/straight.el#customizing-when-packages-are-built
+(setq straight-check-for-modifications '(find-when-checking))
+
+;; Standard bootstrap preamble from https://github.com/raxod502/straight.el#bootstrapping-straightel
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
