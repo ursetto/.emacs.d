0e2bbdf62522d5532132b9e6737c807d0267999e
Jim Ursetto <jim@3e8.org>
2021-04-28 00:11:01 -0500

    Add idle-highlight-mode package, used by python/scheme

--- init.el
+++ init.el
@@ -125,6 +125,7 @@
 (use-package discover-my-major
   :bind (("C-h C-m" . discover-my-major)
          ("C-h M-m" . discover-my-mode)))
+(use-package idle-highlight-mode :defer t)
 
 ;;; Initialization
 ;;;; Main
