6d37e6d67ed4c57d84292af795ba5a7e293a1a68
Jim Ursetto <jim@3e8.org>
2022-02-10 17:52:18 -0600

    more lsp config, avoid auto lsp startup first time

--- init/init-rust.el
+++ init/init-rust.el
@@ -21,6 +21,16 @@
 ;; TODO: If *lsp-help* buffer is open in a window (M-l h h), try to get documentation dynamically
 ;; updated there, instead of using the lsp-ui-doc overlay.
 
+;; Note: If you skip importing a project root ("d"), and later want to import it, use
+;;       M-x lsp-workspace-blacklist-remove. State is stored in ~/.emacs.d/.lsp-session-v1.
+;;
+;; Note: It might be nice to avoid activating lsp for a workspace automatically, because it may compile
+;;       hundreds of megs of code. Once the server is running though, it can be enabled without penalty.
+;;       Putting projects on the blacklist (maybe temporarily) is not a great solution.
+;;       But the lsp backend code is not set up for this (or I can't understand it enough).
+;;       As a workaround, we avoid starting lsp-mode until it has been explicitly invoked once via M-x lsp.
+;;       - Also note: `M-l s s` is bound to `lsp`, but pointless (?)--can't use until lsp is active in buffer.
+
 (use-package cargo
   :hook (rust-mode . cargo-minor-mode))
 
@@ -56,6 +66,7 @@
   (lsp-rust-analyzer-cargo-watch-command "clippy")
   (lsp-rust-server 'rust-analyzer) ;; rust-analyzer or rls
   (lsp-enable-symbol-highlighting nil) ;; Activate with M-l a h
+  (read-process-output-max (* 1024 1024))
   :config
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
   :hook (rust-mode . (lambda ()
@@ -63,14 +74,22 @@
                        ;; as generic VC mode is usually disabled globally for performance.
                        (if (null vc-handled-backends)
                            (setq-local vc-handled-backends '(Git)))
-                       (lsp))))
+                       ;; Avoid invoking lsp until the user explicitly has done it once via `M-x lsp`.
+                       ;; Not a perfect solution--would prefer a per-*workspace* activation--but this does
+                       ;; prevent starting up by accident when you are just browsing.
+                       (when (fboundp 'lsp-mode)
+                         (lsp))
+                       )))
 
 (use-package lsp-ui
   :defer t
   :commands lsp-ui-mode
   :custom
-  (lsp-ui-doc-enable nil)  ; toggle on with M-l T d
+  (lsp-ui-doc-enable t)  ; toggle with M-l T d
   (lsp-ui-doc-include-signature t) ; Include signature for rls. Has no effect for rust-analyzer.
+  (lsp-ui-doc-max-height 25)
+  (lsp-ui-sideline-show-hover nil)  ; Semi-useful but cluttered. With rust-analyzer, often spams screen with doc text.
+  (lsp-ui-sideline-show-code-actions nil)
   )
 
 (provide 'init-rust)
