43110d12d91dfa4770065d287c5ade2991cca03a
Jim Ursetto <jim@3e8.org>
2022-06-03 20:04:54 -0500

    Use rgb hex values for tty colors

--- early-init.el
+++ early-init.el
@@ -16,19 +16,41 @@
             ;; use this on 256-color terminals without palette remapping.
             ;; You may also map to an existing color with (cdr (assoc "salmon" color-name-rgb-alist)).
             (if (> (display-color-cells) 256)
-                (progn
-                  (tty-color-define "red"            1 '(59392 20736 18688))    ;; #e85149
-                  (tty-color-define "blue"           4 '(    0 22528 59392))    ;; #0058e8
-                  (tty-color-define "magenta"        5 '(50944 25088 54272))    ;; #c762d4
-                  (tty-color-define "brightred"      9 '(65535 29696 29184))    ;; #ff7472
-                  (tty-color-define "brightgreen"   10 '(24320 63744 26368))    ;; #5ff967
-                  (tty-color-define "brightyellow"  11 '(65024 64256 17152))    ;; #fefb43
-                  (tty-color-define "brightblue"    12 '(23552 38656 65535))    ;; #5c97ff
-                  (tty-color-define "brightmagenta" 13 '(65535 39680 65535))    ;; #ff9bff
-                  (tty-color-define "brightcyan"    14 '(24320 64768 65280))    ;; #5ffdff
+                (progn    ; can use cl-flet to abbrev functions https://emacs.stackexchange.com/a/33899/28382
+                  (tty-color-define "red"            1 (zb/rgb-to-triple "#e85149"))
+                  (tty-color-define "yellow"         3 (zb/rgb-to-triple "#e0c200"))
+                  (tty-color-define "blue"           4 (zb/rgb-to-triple "#0058e8"))
+                  (tty-color-define "magenta"        5 (zb/rgb-to-triple "#c762d4"))
+                  (tty-color-define "brightred"      9 (zb/rgb-to-triple "#ff7472"))
+                  (tty-color-define "brightgreen"   10 (zb/rgb-to-triple "#5ff967"))
+                  (tty-color-define "brightyellow"  11 (zb/rgb-to-triple "#fefb43"))
+                  (tty-color-define "brightblue"    12 (zb/rgb-to-triple "#5c97ff"))
+                  (tty-color-define "brightmagenta" 13 (zb/rgb-to-triple "#ff9bff"))
+                  (tty-color-define "brightcyan"    14 (zb/rgb-to-triple "#5ffdff"))
                   )
               )
             ))
+
+;; Convert #XXYYZZ or #XYZ to (R G B), the triple expected by tty-color-define.
+;; Swiped from tty-color-translate.
+(defun zb/rgb-to-triple (color)
+  (and (stringp color)
+       (>= (length color))
+       (eq (aref color 0) ?#)
+       (member (aref color 1)
+               '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9
+		    ?a ?b ?c ?d ?e ?f))
+       (let* ((ndig (/ (- (length color) 1) 3))
+			      (i1 1)
+			      (i2 (+ i1 ndig))
+			      (i3 (+ i2 ndig)))
+         (list
+          (lsh (string-to-number (substring color i1 i2) 16)
+	       (+ 8 (* 4 (- 2 ndig))))
+	  (lsh (string-to-number (substring color i2 i3) 16)
+	       (+ 8 (* 4 (- 2 ndig))))
+	  (lsh (string-to-number (substring color i3) 16)
+	       (+ 8 (* 4 (- 2 ndig))))))))
 
 ;; early-init's main purpose is to set up the package manager.  In Emacs >= 27, that's
 ;; done implicitly at the end of early-init, but in earlier versions it happens at the end
